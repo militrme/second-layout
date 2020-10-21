fbee63b69833236fc8cf8308d7256fc13548d1c0
militrme <militrme@mail.ru>
2020-10-21 14:46:43 +0300

    slider

---
+++ src/script/slider.js
@@ -0,0 +1,16 @@
+$('.slider-for').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor:'.'
+});
+
+$('.slider-for').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    focusOnSelect: true,
+    asNavFor:'.'
+});
